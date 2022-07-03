9b1643c3884bb55b3a20b3169698f847480cd189
ChetanGavanali <chetangavanali@gmail.com>
2022-07-03 19:14:27 +0530

    Added: Algorithm Problems

---
+++ src/com/bridgelabz/InsertionSort.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class InsertionSort {
+    public static String[] sort(String array[], int length) {
+        String temp = " ";
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (array[i].compareToIgnoreCase(array[j]) > 0) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = { "a", "ab", "abc", "abcd", "abcde", "abcdef", "abcdefg", "abcdefgh" };
+        String sortedArray[] = sort(arr, arr.length);
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.println(sortedArray[i]);
+        }
+    }
+}
